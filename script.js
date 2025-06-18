6a4aa99c00830ed1f67b0a88ebb086072ca41485
saikirananumalla <saikirananumalla@gmail.com>
2025-06-18 15:43:29 -0400

    remove particles on name

--- script.js
+++ script.js
@@ -80,35 +80,6 @@ gsap.utils.toArray(".journey-card").forEach((card, index) => {
   });
 });
 
-// HERO SECTION PARTICLES
-particlesJS("particles-hero", {
-  "particles": {
-    "number": {
-      "value": 200,
-      "density": { "enable": true, "value_area": 800 }
-    },
-    "color": { "value": "#ffffff" },
-    "opacity": { "value": 0.7, "random": false },
-    "size": { "value": 4, "random": true },
-    "line_linked": { "enable": false },
-    "move": {
-      "enable": true,
-      "speed": 1,
-      "direction": "bottom",
-      "out_mode": "out"
-    }
-  },
-  "interactivity": {
-    "events": {
-      "onhover": { "enable": false },
-      "onclick": { "enable": false }
-    }
-  },
-  "retina_detect": true
-});
-
-
-
 particlesJS("particles-projects", {
   "particles": {
     "number": { "value": 50 },
